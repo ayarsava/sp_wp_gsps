79f182ab8314580f751dcd0caa0e9c715150ba0a
Ayar Sava <ayar.sava@cultura.gob.ar>
2021-07-07 17:11:03 -0300

    REV: Front page resources x6

--- wp-content/plugins/gsps/gsps-plugin.php
+++ wp-content/plugins/gsps/gsps-plugin.php
@@ -687,3 +687,60 @@ function gsps_enqueue_front_page_scripts() {
     }
 }
 add_action( 'wp_enqueue_scripts', 'gsps_enqueue_front_page_scripts' );
+
+
+
+add_action('admin_init', 'gsps_general_section');  
+function gsps_general_section() {  
+    add_settings_section(  
+        'gsps_settings_section', // Section ID 
+        'Migration pathways Map', // Section Title
+        'my_section_options_callback', // Callback
+        'general' // What Page?  This makes the section show up on the General Settings Page
+    );
+
+    add_settings_field( // Map url
+        'map_url', // Option ID
+        'Map url', // Label
+        'my_urlbox_callback', // !important - This is where the args go!
+        'general', // Page it will be displayed (General Settings)
+        'gsps_settings_section', // Name of our section
+        array( // The $args
+            'map_url' // Should match Option ID
+        )  
+    ); 
+
+    add_settings_field( // Map text
+        'map_text', // Option ID
+        'Map text', // Label
+        'my_textbox_callback', // !important - This is where the args go!
+        'general', // Page it will be displayed
+        'gsps_settings_section', // Name of our section (General Settings)
+        array( // The $args
+            'map_text' // Should match Option ID
+        )  
+    ); 
+
+    register_setting('general','map_url', 'esc_attr');
+    register_setting('general','map_text','');
+}
+
+function my_section_options_callback() { // Section Callback
+    echo '<p>Enter map url and legal or explanatory text.</p>';  
+}
+
+function my_urlbox_callback($args) {  // Textbox Callback
+    $option = get_option($args[0]);
+    echo '<input type="text" id="'. $args[0] .'" name="'. $args[0] .'" value="' . $option . '" />';
+}
+function my_textbox_callback($args) {  // Textbox Callback
+    $option = get_option($args[0]);
+    echo wp_editor( 
+        $option, 
+        'map_text', 
+        array( 'textarea_name' => 'map_text' ) 
+    );
+}
+
+
+
